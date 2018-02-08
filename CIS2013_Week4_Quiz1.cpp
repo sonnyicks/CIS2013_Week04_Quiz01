94f6048e327ea1fee04bcaf29c56e65fa260cfb9
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-02-07 18:20:32 -0600

    Quiz 1 for week 4

--- CIS2013_Week4_Quiz1.cpp
+++ CIS2013_Week4_Quiz1.cpp
@@ -6,6 +6,9 @@ int main()
 	int num_loop = 1;
 	bool run_again = true;
 	while (num_loop <= 2000) {
+		if ((num_loop % 50 != 0) && (num_loop % 33 != 0)) {
+			cout << num_loop << endl;
+		}
 		if (num_loop % 50 == 0) {
 			cout << num_loop << " FOO";
 			if (num_loop % 33 == 0) {
