b8cc13770169d7e3530c04aea7eaab179ef5d995
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-02-07 18:52:57 -0600

    Week 4 quiz
    Numbers of Foos and Bars added

--- CIS2013_Week4_Quiz1.cpp
+++ CIS2013_Week4_Quiz1.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main()
 {
 	int num_loop = 1;
+	int num_bars = 0;
+	int num_foos = 0;
+
 	bool run_again = true;
 	while (num_loop <= 2000) {
 		if ((num_loop % 50 != 0) && (num_loop % 33 != 0)) {
@@ -11,16 +14,20 @@ int main()
 		}
 		if (num_loop % 50 == 0) {
 			cout << num_loop << " FOO";
+			(num_foos++);
 			if (num_loop % 33 == 0) {
 				cout << " BAR";
+				(num_bars++);
 			}
 			cout << endl;
 		}
 		if ((num_loop % 33 == 0) && (num_loop %50 !=0)) {
 			cout<< num_loop << " BAR" <<endl;
+			(num_bars++);
 		}
 		num_loop = num_loop + 1;
 	}
+	cout << "There are " << num_foos << " Foos and " << num_bars << " Bars." << endl;
 	cin >> run_again;
 	return 0;
 }
